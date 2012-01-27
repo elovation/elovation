b1cffa88c4dc14480ec6c3fd33db8ca7c8e14969
drew <drew@drewolson.org>
2012-01-27 16:54:05 -0600

    Dashboard - Add Player create

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -2,4 +2,12 @@ class PlayersController < ApplicationController
   def new
     @player = Player.new
   end
+
+  def create
+    @player = Player.new(params[:player])
+
+    if @player.save
+      redirect_to dashboard_path
+    end
+  end
 end
--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -8,4 +8,14 @@ describe PlayersController, :type => :controller do
       assigns(:player).should_not be_nil
     end
   end
+
+  describe "create" do
+    it "creates a player given valid params" do
+      post :create, :player => {:name => "Drew"}
+
+      response.should redirect_to(dashboard_path)
+
+      Player.where(:name => "Drew").first.should_not be_nil
+    end
+  end
 end
