0cacb4e87cae1e2cafaa7ae308f103f2e05ff4ad
drew <drew@drewolson.org>
2012-01-27 16:08:23 -0600

    skeleton - Update root route

--- config/routes.rb
+++ config/routes.rb
@@ -46,9 +46,7 @@ Elovation::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'dashboard#show'
 
   # See how all your routes lay out with "rake routes"
 
