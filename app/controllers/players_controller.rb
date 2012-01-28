c795212eced9b263d03700d43ef27acba11e478a
drew <drew@drewolson.org>
2012-01-28 16:22:18 -0600

    Add ability to edit/update/delete players

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,5 @@
 class PlayersController < ApplicationController
-  def new
-    @player = Player.new
-  end
+  before_filter :_find_player, :only => [:destroy, :edit, :update]
 
   def create
     @player = Player.new(params[:player])
@@ -11,5 +9,30 @@ class PlayersController < ApplicationController
     else
       render :new
     end
+  end
+
+  def destroy
+    @player.destroy
+
+    redirect_to dashboard_path
+  end
+
+  def edit
+  end
+
+  def new
+    @player = Player.new
+  end
+
+  def update
+    if @player.update_attributes(params[:player])
+      redirect_to dashboard_path
+    else
+      render :edit
+    end
+  end
+
+  def _find_player
+    @player = Player.find(params[:id])
   end
 end
--- spec/controllers/players_controller_spec.rb
+++ spec/controllers/players_controller_spec.rb
@@ -26,4 +26,62 @@ describe PlayersController do
       response.should render_template(:new)
     end
   end
+
+  describe "edit" do
+    it "exposes the player for editing" do
+      player = FactoryGirl.create(:player)
+
+      get :edit, :id => player
+
+      assigns(:player).should == player
+    end
+  end
+
+  describe "update" do
+    context "with valid params" do
+      it "redirects to the dashboard page" do
+        player = FactoryGirl.create(:player, :name => "First name")
+
+        put :update, :id => player, :player => {:name => "Second name"}
+
+        response.should redirect_to(dashboard_path)
+      end
+
+      it "updates the player with the provided attributes" do
+        player = FactoryGirl.create(:player, :name => "First name")
+
+        put :update, :id => player, :player => {:name => "Second name"}
+
+        player.reload.name.should == "Second name"
+      end
+    end
+
+    context "with invalid params" do
+      it "renders the edit page" do
+        player = FactoryGirl.create(:player, :name => "First name")
+
+        put :update, :id => player, :player => {:name => nil}
+
+        response.should render_template(:edit)
+      end
+    end
+  end
+
+  describe "destroy" do
+    it "redirects to the dashboard path" do
+      player = FactoryGirl.create(:player)
+
+      delete :destroy, :id => player
+
+      response.should redirect_to(dashboard_path)
+    end
+
+    it "deletes the given player" do
+      player = FactoryGirl.create(:player)
+
+      delete :destroy, :id => player
+
+      Player.find_by_id(player.id).should be_nil
+    end
+  end
 end
