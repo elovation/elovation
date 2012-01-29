31b2ac8cd9ed979f457db6c4facd1d7be3ff132b
drew <drew@drewolson.org>
2012-01-29 10:50:53 -0600

    Display recent results on player pages

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,4 +2,8 @@ class Player < ActiveRecord::Base
   has_and_belongs_to_many :results
 
   validates :name, :uniqueness => true, :presence => true
+
+  def recent_results
+    results.order("created_at DESC").limit(5)
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -1,14 +1,6 @@
 require "spec_helper"
 
 describe Player do
-  describe "name" do
-    it "has a name" do
-      player = FactoryGirl.create(:player, :name => "Drew")
-
-      player.name.should == "Drew"
-    end
-  end
-
   describe "validations" do
     context "name" do
       it "is required" do
@@ -25,6 +17,57 @@ describe Player do
         player.should_not be_valid
         player.errors[:name].should == ["has already been taken"]
       end
+    end
+  end
+
+  describe "name" do
+    it "has a name" do
+      player = FactoryGirl.create(:player, :name => "Drew")
+
+      player.name.should == "Drew"
+    end
+  end
+
+  describe "recent_results" do
+    it "returns 5 of the player's results" do
+      game = FactoryGirl.create(:game)
+      player = FactoryGirl.create(:player)
+
+      10.times { FactoryGirl.create(:result, :game => game, :winner => player) }
+
+      player.recent_results.size.should == 5
+    end
+
+    it "returns the 5 most recently created results" do
+      newer_results = nil
+      game = FactoryGirl.create(:game)
+      player = FactoryGirl.create(:player)
+
+      Timecop.freeze(3.days.ago) do
+        5.times.map { FactoryGirl.create(:result, :game => game, :winner => player) }
+      end
+
+      Timecop.freeze(1.day.ago) do
+        newer_results = 5.times.map { FactoryGirl.create(:result, :game => game, :winner => player) }
+      end
+
+      player.recent_results.sort.should == newer_results.sort
+    end
+
+    it "orders results by created_at, descending" do
+      game = FactoryGirl.create(:game)
+      player = FactoryGirl.create(:player)
+      old = new = nil
+
+      Timecop.freeze(2.days.ago) do
+        old = FactoryGirl.create(:result, :game => game, :winner => player)
+      end
+
+      Timecop.freeze(1.days.ago) do
+        new = FactoryGirl.create(:result, :game => game, :winner => player)
+      end
+
+      player.recent_results.should == [new, old]
     end
   end
 end
