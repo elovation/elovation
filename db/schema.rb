64c0fcc8a82fc9b7f0727a43936c44ecfdf033d8
Drew Olson <drew@drewolson.org>
2012-02-06 16:45:43 -0600

    Update .gitignore and db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 100) do
+ActiveRecord::Schema.define(:version => 1) do
 
   create_table "games", :force => true do |t|
     t.string   "name",       :null => false
