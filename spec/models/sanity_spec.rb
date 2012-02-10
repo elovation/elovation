691d587c0d108ffa84c0719d672153da0cccdadd
drew <drew@drewolson.org>
2012-02-10 11:23:44 -0600

    Freeze migration 2

--- spec/models/sanity_spec.rb
+++ spec/models/sanity_spec.rb
@@ -4,7 +4,8 @@ describe "sanity" do
   describe "frozen mirations" do
     it "ensures deployed migrations have not changed" do
       migrations_digests = {
-        "001_release_001.rb" => "55d3fa3763a8ebaac18b8c1a9ff7f053"
+        "001_release_001.rb" => "55d3fa3763a8ebaac18b8c1a9ff7f053",
+        "002_release_002.rb" => "983bbaf2307fd7c00f34a2242e5498b2"
       }
 
       migrations_digests.each do |migration, digest|
