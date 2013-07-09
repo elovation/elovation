9242e815968e6b51f077aac491e1f5868d36193e
phillc <spyyderz@gmail.com>
2013-07-08 22:31:16 -0400

    Repairing/replaying results to fix any rating issues

--- app/models/game.rb
+++ app/models/game.rb
@@ -66,4 +66,13 @@ class Game < ActiveRecord::Base
   def rater
     RATER_MAPPINGS[rating_type]
   end
+
+  def recalculate_ratings!
+    RatingHistoryEvent.joins(:rating).where(ratings: {game_id: self.id}).destroy_all
+    Rating.where(game_id: self.id).destroy_all
+
+    results.order("id ASC").all.each do |result|
+      rater.update_ratings self, result.teams.order("rank ASC")
+    end
+  end
 end
