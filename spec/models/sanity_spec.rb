26c0fc1d27d280ea51e834a12c84a8300b654c2a
Miha Rekar <info@mr.si>
2014-07-28 10:19:38 +0200

    change md5 sanity hashes

--- spec/models/sanity_spec.rb
+++ spec/models/sanity_spec.rb
@@ -4,8 +4,8 @@ describe "sanity" do
   describe "frozen mirations" do
     it "ensures deployed migrations have not changed" do
       migrations_digests = {
-        "001_release_001.rb" => "55d3fa3763a8ebaac18b8c1a9ff7f053",
-        "002_release_002.rb" => "983bbaf2307fd7c00f34a2242e5498b2",
+        "001_release_001.rb" => "a893705e0162076ba9416f805b13362e",
+        "002_release_002.rb" => "ee6c0915268eca0f8a317c1a7e2048be",
         "003_release_003.rb" => "ef70f4e0dd6214f426fdd162d56e88c4",
         "004_release_004.rb" => "152f9acaad9708d938d0d797084c2687"
       }
