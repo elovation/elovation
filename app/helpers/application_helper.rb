3d5188ea9e492130d7de13a6922b704eff832719
Christos Hrousis <christos.hrousis@gmail.com>
2023-09-05 20:11:26 +1000

    :sparkles: Restore gravatar and title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
   def format_time(time)
     "#{time_ago_in_words(time)} ago"
   end
+
+  def gravatar_url(player, options = {})
+    options.assert_valid_keys :size
+    size = options[:size] || 32
+    digest = player.email.blank? ? "0" * 32 : Digest::MD5.hexdigest(player.email)
+    "http://www.gravatar.com/avatar/#{digest}?d=mm&s=#{size}"
+  end
+
+  def brand_title
+    ENV.fetch("ELOVATION_TITLE", "Elovation")
+  end
 end
