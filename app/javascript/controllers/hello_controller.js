072a9f5aab825f28ce55a88250abb99036295485
Christos Hrousis <christos.hrousis@gmail.com>
2023-09-05 22:18:34 +1000

    :sparkles: Inline new game form :sparkles:

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
+  static targets = [ "name" ]
+
+  greet() {
+    console.log(`Hello, ${this.name}!`)
+  }
+
+  get name() {
+    return this.nameTarget.value
   }
 }
