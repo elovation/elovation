c2df348252e2f6f7cc46625fb9d10d9183766cf5
Christos Hrousis <christos.hrousis@gmail.com>
2023-09-07 19:08:45 +1000

    :art: Request image via robohash for fallback robots

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,8 +12,13 @@ module ApplicationHelper
   def gravatar_url(player, options = {})
     options.assert_valid_keys :size
     size = options[:size] || 32
-    digest = player.email.blank? ? "0" * 32 : Digest::MD5.hexdigest(player.email)
-    "http://www.gravatar.com/avatar/#{digest}?d=mm&s=#{size}"
+    if player.email.blank?
+      digest = Digest::MD5.hexdigest(player.email)
+      "https://robohash.org/#{digest}?size=#{size}x#{size}"
+    else
+      digest = Digest::MD5.hexdigest(player.email)
+      "https://robohash.org/#{digest}?gravatar=hashed&size=#{size}x#{size}"
+    end
   end
 
   def brand_title
