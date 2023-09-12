a66a9a7367a714eb41c9373b6a367636122de2ad
Christos Hrousis <christos.hrousis@gmail.com>
2023-09-12 23:03:57 +1000

    :fire: Remove minitest generated files

--- test/controllers/games_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class GamesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/rating_history_event_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class RatingHistoryEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
